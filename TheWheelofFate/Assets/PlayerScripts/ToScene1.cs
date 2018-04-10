f669dbbf6f09f74b5dc71a54a2ca569c960006b6
SonodaYomi <625522391@qq.com>
2018-04-10 19:18:22 +0800

    异步读取及读取界面
    在主界面上放了个按钮，进入到场景1，由一个异步加载的loading场景过渡。

---
+++ TheWheelofFate/Assets/PlayerScripts/ToScene1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToScene1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadSceneOne()
+    {
+        Globe.nextSceneName = "Scene1";
+        SceneManager.LoadScene(1);
+    }
+    
+}
